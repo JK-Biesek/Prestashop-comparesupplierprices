df0465157adbd9573dc1098f29a0d0468f6be94a
Jakub Biesek <36658591+JK-Biesek@users.noreply.github.com>
2018-03-06 10:58:04 +0100

    Loop through products from cart
    get values of products inside cart

--- comparesupplierprices.php
+++ comparesupplierprices.php
@@ -41,6 +41,27 @@ class CompareSupplierPrices extends Module {
       return false;
     return true;
   }
+    
+public function hookDisplayAdminOrder($params) {
+ 
+    $order = new Order((int) Tools::getValue('id_order'));
+    $cart = new Cart($order->id_cart);
+    $products_temp = $cart->getProducts();
+    $products= array();
+    $id_customer = $this->context->customer->id;
+    
+    foreach ($products_temp => $val) {
 
-
-}
+          $product = array();
+          $product['id_product'] = $val['id_product'];
+          $product['name'] = $val['name'];
+          $product['quantity'] = $val['quantity'];
+          $product['price'] = $val['price'];
+          $product['supplier_reference'] = $val['supplier_reference'];
+          $product['id_supplier'] = $val['id_supplier'];
+          $details = $this->getOrderRowDetails((int) Tools::getValue('id_order'), $val['id_product'], $val['id_product_attribute']);
+          $product['wholesale_price'] = $details['purchase_supplier_price'];
+          $product['attr_name'] = $val['attributes'];
+    }   
+  }
+ }
